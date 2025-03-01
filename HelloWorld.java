82a86d5e3318c5535fe51e1967a8b9a86494a1e6
jason <jentle_81@163.com>
2025-03-01 23:06:46 +0800

    add multiplication to helloword

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
         int sum = add(5, 3);
         System.out.println("Sum: " + sum);
         int difference = minus(5, 3);
-        System.out.println("Difference: " + difference);        
+        System.out.println("Difference: " + difference);
+        int product = multiply(5, 3);
+        System.out.println("Product: " + product);        
     }
 
     public static int add(int a, int b) {
@@ -17,6 +19,13 @@ public class HelloWorld {
         System.out.println("Starting minus function with parameters: " + a + ", " + b);
         int result = a - b;
         System.out.println("Ending minus function with result: " + result);
+        return result;
+    }
+
+    public static int multiply(int a, int b) {
+        System.out.println("Starting multiply function with parameters: " + a + ", " + b);
+        int result = a * b;
+        System.out.println("Ending multiply function with result: " + result);
         return result;
     }
 }

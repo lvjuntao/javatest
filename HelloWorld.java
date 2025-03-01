b4cfc4a4ffcb5a91604f9975347a8ffdcd0ae2dc
jason <jentle_81@163.com>
2025-03-01 22:37:35 +0800

    add minus function to helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,20 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         int sum = add(5, 3);
         System.out.println("Sum: " + sum);
+        int difference = minus(5, 3);
+        System.out.println("Difference: " + difference);        
     }
 
     public static int add(int a, int b) {
         System.out.println("Starting add function with parameters: " + a + ", " + b);
         int result = a + b;
         System.out.println("Ending add function with result: " + result);
+        return result;
+    }
+    public static int minus(int a, int b) {
+        System.out.println("Starting minus function with parameters: " + a + ", " + b);
+        int result = a - b;
+        System.out.println("Ending minus function with result: " + result);
         return result;
     }
 }
